5834d4a91733d27427c53257fd10b4ea93f5318e
Marco Sameh <marcosameh678@gmail.com>
2022-09-03 19:39:02 +0200

    Lazzaya Alert

--- App.UI/Models/ContactUsEmail.cs
+++ App.UI/Models/ContactUsEmail.cs
@@ -1,15 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace App.UI.Models
 {
     public class ContactUsEmail
     {
+        [Required]
         public string FullName { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Phone { get; set; }
         public string Country { get; set; }
+        [Required]
         public string Subject { get; set; }
+        [Required]
+        public string Department { get; set; }
+        [Required]
         public string Message { get; set; }
         public string Title { get; set; }
         public string BusinessName { get; set; }
--- App.UI/Pages/Index.cshtml.cs
+++ App.UI/Pages/Index.cshtml.cs
@@ -1,6 +1,12 @@
 ﻿using App.UI.Enums;
+using App.UI.InfraStructure;
+using App.UI.Models;
+using AppCore.DTO;
+using AppCore.Enums;
 using AppCore.Managers;
 using AppCore.Models;
+using LazZiya.TagHelpers.Alerts;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Linq;
 
@@ -12,20 +18,31 @@ namespace App.UI.Pages
         private readonly MenuManager menuManager;
         private readonly EventManager eventManager;
         private readonly GalleryManager galleryManager;
+        private readonly ResourceInfo resourceInfo;
+        private readonly MailManager mailManager;
+        private readonly BusinessInfoManager businessInfoManager;
 
         public MediaItem TopRightMedia { get; set; }
         public MediaItem RightImageMedia { get; set; }
         public MediaItem LeftImageMedia { get; set; }
         public IQueryable<MenuSection> Menu { get; set; }  
         public IQueryable<Event> Events { get; set; }  
-        public IQueryable<Gallery> Galleries { get; set; }  
+        public IQueryable<Gallery> Galleries { get; set; }
+
+        [BindProperty]
+        public ContactUsEmail contactUsEmail { get; set; } = new ContactUsEmail();
+
         public IndexModel(MediaItemManager mediaItemManager,
-            MenuManager menuManager,EventManager eventManager,GalleryManager galleryManager)
+            MenuManager menuManager,EventManager eventManager,GalleryManager galleryManager,
+            ResourceInfo resourceInfo,MailManager mailManager,BusinessInfoManager businessInfoManager)
         {
             this.mediaItemManager = mediaItemManager;
             this.menuManager = menuManager;
             this.eventManager = eventManager;
             this.galleryManager = galleryManager;
+            this.resourceInfo = resourceInfo;
+            this.mailManager = mailManager;
+            this.businessInfoManager = businessInfoManager;
         }
 
 
@@ -44,6 +61,35 @@ namespace App.UI.Pages
             Menu=menuManager.GetMenu();
             Events = eventManager.GetEvents();
             Galleries=galleryManager.GetGallery();
+
+        }
+        public IActionResult OnPost()
+        {
+            Initialize();
+
+
+            MailInfo mailInfo = new MailInfo
+                {
+                    Title = "Contact Form Request",
+                    Model =contactUsEmail,
+                    ToMail = businessInfoManager.GetSetting(SettingType.ContactUsEmail),
+                    Subject = contactUsEmail.Subject,
+                    TemplateName = "ContactUs",
+                };
+                var result = mailManager.SendRequest(mailInfo);
+
+
+
+                if (result.IsFailure)
+                {
+                TempData.Warning(result.Error, "SomeThingWrong", false);
+            }
+
+            TempData.Success("Your Email Send Successfly We will Contact You Soon");
+            return Page();
+
+
+
 
         }
 
